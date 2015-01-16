30d00198b0528fcfa62c29e145a2e44d2839a528
ansor <ansor@mail.puncaktegap.com.my>
2015-01-16 16:15:23 +0800

    first commit

---
+++ src/com/test/robot/ToyRobot.java
@@ -0,0 +1,12 @@
+/* COPYRIGHT (C) 2015 Puncak Tegap Sdn Bhd. All Rights Reserved. */
+
+package com.test.robot;
+
+/**
+ * @author Domu Ansor Harahap
+ */
+public class ToyRobot {
+    public static void main(final String[] args) {
+        // TODO robot
+    }
+}
